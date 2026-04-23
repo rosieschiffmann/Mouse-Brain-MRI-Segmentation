62c3a11cd889f87dc434b27b518a10bd52901731
Christiana Routsi <chrissyr.505@gmail.com>
2026-04-23 15:14:49 +0100

    Script to run template construction by ANTs

---
+++ ants/bash_commands/atlas_generation.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Define your paths (Change these to your actual folders)
+INPUT_DIR="/home/user/project/raw_data"
+OUTPUT_DIR="/home/user/project/processed/atlas"
+
+# Create the output directory if it doesn't exist
+mkdir -p "$OUTPUT_DIR"
+
+# Run the command 
+# We use the full path for the output prefix and the input files
+antsMultivariateTemplateConstruction2.sh \
+  -d 3 \
+  -i 5 \
+  -o "${OUTPUT_DIR}/template_" \
+  "${INPUT_DIR}/*.nii"
