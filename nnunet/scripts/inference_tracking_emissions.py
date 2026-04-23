6d162d9fab202544c4001ac60eec64aba8fe061a
Rosie Schiffmann <rosie.schiffmann@gmail.com>
2026-04-23 11:36:11 +0100

    added comments

--- nnunet/scripts/inference_tracking_emissions.py
+++ nnunet/scripts/inference_tracking_emissions.py
@@ -16,7 +16,7 @@ command = (
     'nnUNetv2_predict '
     '-i "C:\\Users\\MBCX7BD2\\mastersproject\\test_images\\new_data_scaled" '    # Test images
     '-o "C:\\Users\\MBCX7BD2\\mastersproject\\inferences\\new_data_scaled\\2d" '    # Output folder
-    '-d 1 -c 2d -f 0 1 2 3 4 -chk checkpoint_best.pth'
+    '-d 1 -c 2d -f 0 -chk checkpoint_best.pth' # Dataset ID, config, fold 0 only, using best checkpoint
 )
 
 # Tell Python to run the command in the terminal
