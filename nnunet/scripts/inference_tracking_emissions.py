0a6a240325ee1579079d0ba527d70748c37bde8b
Rosie Schiffmann <rosie.schiffmann@gmail.com>
2026-04-23 10:55:57 +0100

    changed the filename to reflect emission tracking

--- nnunet/scripts/inference_tracking_emissions.py
+++ nnunet/scripts/inference_tracking_emissions.py
@@ -4,6 +4,7 @@ import subprocess
 # Initialize the tracker 
 tracker = EmissionsTracker(
     project_name="nnUNet_2D_new_data_LabPC", 
+    # Enter your desired output folder for emissions data
     output_dir=r"C:\Users\MBCX7BD2\mastersproject\emissions\new_data_scaled"
 )
 
@@ -13,13 +14,13 @@ tracker.start()
 # Perfrom Inference
 command = (
     'nnUNetv2_predict '
-    '-i "C:\\Users\\MBCX7BD2\\mastersproject\\test_images\\new_data_scaled" '
-    '-o "C:\\Users\\MBCX7BD2\\mastersproject\\inferences\\new_data_scaled\\2d" '
+    '-i "C:\\Users\\MBCX7BD2\\mastersproject\\test_images\\new_data_scaled" '    # Test images
+    '-o "C:\\Users\\MBCX7BD2\\mastersproject\\inferences\\new_data_scaled\\2d" '    # Output folder
     '-d 1 -c 2d -f 0 1 2 3 4 -chk checkpoint_best.pth'
 )
 
 # Tell Python to run the command in the terminal
-print("Running 3D nnU-Net prediction...")
+print("Running nnU-Net prediction...")
 subprocess.run(command, shell=True)
 
 # Stop the tracker once the command finishes
@@ -27,5 +28,4 @@ emissions = tracker.stop()
 
 print("=========================================")
 print(f"DONE! Total Emissions: {emissions:.6f} kg CO2eq")
-print("Data saved to C:\\nnUNet_Laptop\\emissions.csv")
 print("=========================================")
